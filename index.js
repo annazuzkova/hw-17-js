f0f1f7567ab557a738124482137cf129ffbcbcc1
AnnaZuzkova <famzu2018@gmail.com>
2024-08-21 21:54:07 +0300

    done

---
+++ index.js
@@ -0,0 +1,21 @@
+//1
+// const text = document.querySelector("#text");
+// const button = document.querySelector("#button");
+// console.log(button);
+// button.textContent = text.textContent;
+//2
+// const photo = document.querySelector("#photo");
+// photo.src =
+//   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSN_QzQxyrlWN9DqQ604MWoWxIO-wUVqqTRLQ&s";
+//3
+// const link = document.querySelector("#link");
+// const img = document.querySelector("#img");
+// link.href = "https://squoosh.app";
+// img.alt = "котік";
+//4
+// перший варіант(не бажаний)
+// const li = document.querySelector("li");
+// li.textContent = "перший варіант";
+// другий варіант(кращий)
+// const li = document.querySelector("#li");
+// li.textContent = "другий варіант";
